cad30b0d56f91f445bba31ef19d1d3ff71aa0c1a
Iyosinator <iyosiasmulugeta@gmail.com>
2025-10-31 22:55:30 +0300

    Starts...

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+// src/main.rs
+
+// Declare the modules that our application will use
+pub mod command_handler;
+pub mod config;
+pub mod database;
+pub mod error;
+pub mod persistence;
+pub mod replication;
+pub mod server;
+pub mod utils;
+
+fn main() {
+    println!("Starting Redis Clone Server...");
+    // You will add server initialization and startup logic here
+}
